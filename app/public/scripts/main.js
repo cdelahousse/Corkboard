60c102393b95e0acecff52ddfaf8f3852fa199f4
Christian Delahousse <christian@delahousse.ca>
2013-08-07 18:23:43 -0400

    more config settings

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -1,8 +1,19 @@
 require.config({
+  baseUrl: '/scripts/',
   paths: {
-    jquery: '../bower_components/jquery/jquery',
+    jquery: '../bower_components/jquery/jquery.min',
+    underscore: '../bower_components/underscore/underscore-min',
+    Backbone: '../bower_components/backbone/backbone-min',
+    templates: '../templates'
   },
   shim: {
+    underscore: {
+      exports: '_'
+    },
+    Backbone: {
+      deps: ["underscore", "jquery"],
+      exports: "Backbone"
+    }
   }
 });
 
