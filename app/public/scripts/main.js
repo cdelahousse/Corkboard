0868d608c0429bdc8cc672080a53cd1fcd9a0928
Christian Delahousse <christian@delahousse.ca>
2013-08-08 11:25:23 -0400

    added require.js text plugin

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -2,8 +2,10 @@ require.config({
   baseUrl: '/scripts/',
   paths: {
     jquery: '../bower_components/jquery/jquery.min',
+    require : '../bower_components/requirejs/require', // Minify?
     underscore: '../bower_components/underscore/underscore-min',
     Backbone: '../bower_components/backbone/backbone-min',
+    text : '../bower_components/requirejs-text/text',
     templates: '../templates'
   },
   shim: {
