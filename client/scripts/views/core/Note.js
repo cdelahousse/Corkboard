81be2c1d7d682154dd3b845e156ef0590064f31f
Christian Delahousse <christian@delahousse.ca>
2013-08-19 15:50:32 -0400

    Removed drag implementation code from note
    Started using behaviour instead

--- client/scripts/views/core/Note.js
+++ client/scripts/views/core/Note.js
@@ -1,7 +1,7 @@
-// View for Note Container, this includes the bezel 
-define(['text!templates/core/noteContainer.html','backbone', 'underscore', 
-    'require', 'utils', 'config' ],
-    function (noteTmpl, Backbone, _ , require, utils, config ) {
+// View for Note Container, this includes the bezel
+define(['text!templates/core/noteContainer.html','behaviours/Drag',
+    'backbone', 'underscore', 'require', 'utils', 'config' ],
+    function (noteTmpl, DragBehaviour, Backbone, _ , require, utils, config ) {
   'use strict';
 
   var NoteContainer = Backbone.View.extend({
@@ -12,7 +12,21 @@ define(['text!templates/core/noteContainer.html','backbone', 'underscore',
     initialize : function () {
 
       this.listenTo(this.model, 'destroy', this.remove);
-      
+
+      //Add drag behaviour
+      this.behaviour = new DragBehaviour(this);
+
+      //Add hook/handlers for different momends
+      this.behaviour.start(function () {
+        this.$el.addClass('highlight');
+      });
+      this.behaviour.end(function () {
+        this.$el.removeClass('highlight');
+      });
+      this.behaviour.cancel(function () {
+        this.$el.removeClass('highlight');
+      });
+
       // Dynamically load view type
       var type = this.model.get('type');
       var viewType = [ config.requireTypeViews + utils.capitalize(type) ];
@@ -29,20 +43,9 @@ define(['text!templates/core/noteContainer.html','backbone', 'underscore',
       }).bind(this));
     },
     render : function () {
-
       this.el.innerHTML = this.template;
-
-      // Add drag behaviour while preserving what should be the value of 'this'
-      // in Dom event handlers
-      var view = this;
-      this.el.addEventListener('drag-start', function () { 
-        // 'this' is DOM element
-        view.drag.call(this, view);
-      });
-        
       return this;
     },
-
     events : {
       'click .nav > .delete' : 'destroy',
       'click .nav > .edit' : 'edit',
@@ -55,45 +58,6 @@ define(['text!templates/core/noteContainer.html','backbone', 'underscore',
     destroy : function () { this.model.destroy(); },
 
     toggleNav : function (){ this.$el.find('.edit,.save').toggleClass('hide');},
-
-    // Note drag behaviour
-    // XXX Modularize and abstract away
-    drag : function (view) {
-
-      var target = this, //DOM elem
-          startOffsetX = view.el.getBoundingClientRect().left,
-          startOffsetY = view.el.getBoundingClientRect().top;
-
-      var transform = typeof target.style.transform === 'string' ?
-        'transform' : 'webkitTransform'; 
-      
-      target.addEventListener('drag-move', move);
-      target.addEventListener('drag-end', end);
-      target.addEventListener('drag-cancel', end);
-
-      function move (e) {
-
-        var dx = e.detail.deltaX;
-        var dy = e.detail.deltaY;
-        view.el.style[ transform ] =
-          'translate(' + dx + 'px, ' + dy + 'px)';
-      }
-
-      function end(e) {
-        var dx = e.detail.deltaX;
-        var dy = e.detail.deltaY;
-        var top = startOffsetY + dy;
-        var left = startOffsetX + dx;
-        view.el.style.top = top + 'px';
-        view.el.style.left = left + 'px';
-        view.el.style.position = 'absolute';
-        view.el.style[ transform ] = '';
-
-        target.removeEventListener('drag-move', move);
-        target.removeEventListener('drag-end', end);
-        target.removeEventListener('drag-cancel', end);
-      }
-    }
   });
 
   return NoteContainer;
